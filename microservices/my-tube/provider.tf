e1729427875033cdf9628ba32812f66fbc84af63
Sentinel <muhamad.salman2001@gmail.com>
2024-09-05 17:42:05 +0200

    moved lb main tf

--- microservices/my-tube/provider.tf
+++ microservices/my-tube/provider.tf
@@ -22,17 +22,17 @@ provider "azurerm" {
   }
 }
 
-provider "kubernetes" {
-  alias = aks
-  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
+# provider "kubernetes" {
+#   alias = aks
+#   host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+#   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+#   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+# }
 
-provider "kubernetes" {
-  alias = eks
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+# provider "kubernetes" {
+#   alias = eks
+#   host                   = aws_eks_cluster.eks.endpoint
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.eks.token
+# }
