e7273927e89a71e36a6be6afab3a4a04b22a8fe1
Sentinel <muhamad.salman2001@gmail.com>
2024-09-09 15:08:49 +0200

    modified storage versice main

--- microservices/my-tube/azure-storage/storage-service-main.tf
+++ microservices/my-tube/azure-storage/storage-service-main.tf
@@ -1,5 +1,34 @@
 variable "image_name" {
   type = string
+  default = "mhs20/mytube:azure-storage-010"
+}
+
+terraform {
+  cloud {
+
+    organization = "Sentinel27-Org"
+
+    workspaces {
+      name = "azure-storage-state"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  // profile = "default"
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 module "storage_service" {
